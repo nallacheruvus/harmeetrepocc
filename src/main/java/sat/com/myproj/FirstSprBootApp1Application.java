a6c71780f573400ee16b5c013c5d8dddf258900e
nallacheruvus <41948894+nallacheruvus@users.noreply.github.com>
2024-09-01 22:11:06 +0530

    Fourth commit

--- src/main/java/sat/com/myproj/FirstSprBootApp1Application.java
+++ src/main/java/sat/com/myproj/FirstSprBootApp1Application.java
@@ -1,13 +1,17 @@
 package sat.com.myproj;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FirstSprBootApp1Application {
-
+public class FirstSprBootApp1Application implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(FirstSprBootApp1Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Harmeets Service is up and running well!");
+	}
 }
